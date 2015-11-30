9d8e687cbf116447649a2cef23e480c7986e6a73
Peter Braden <peterbraden@peterbraden.co.uk>
2015-11-30 20:07:29 +0100

    more stressful scene

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,17 +1,17 @@
 #include "scene.h"
 
 Scene* initScene (){
-  int width = 10;
-  int height = 10;
+  int width = 360;
+  int height = 240;
 
   SceneGraph* demoObjs = new SceneGraph();
 
-  Sphere *a = new Sphere((vec3) {4.,0.,0.}, 2., (Color) { 0, 255, 0});
-  Sphere *b = new Sphere((vec3) {-4.,0.,0.}, 2., (Color) {0, 0, 255});
-  Sphere *c = new Sphere((vec3) {0.,4.,0.}, 2., (Color) {255, 0, 0});
-  Sphere *d = new Sphere((vec3) {0.,-4.,0.}, 2., (Color) {255, 0, 0});
-  Sphere *e = new Sphere((vec3) {0.,0.,4.}, 2., (Color) {255, 0, 0});
-  Sphere *f = new Sphere((vec3) {0.,0.,-4.}, 2., (Color) {255, 0, 0});
+  Sphere *a = new Sphere((vec3) {4.,4.,0.}, 2., (Color) { 0, 255, 0});
+  Sphere *b = new Sphere((vec3) {-4.,4.,0.}, 2., (Color) {0, 0, 255});
+  Sphere *c = new Sphere((vec3) {0.,8.,0.}, 2., (Color) {255, 0, 0});
+  Sphere *d = new Sphere((vec3) {0.,0.,0.}, 2., (Color) {255, 0, 0});
+  Sphere *e = new Sphere((vec3) {0.,4.,4.}, 2., (Color) {255, 0, 0});
+  Sphere *f = new Sphere((vec3) {0.,4.,-4.}, 2., (Color) {255, 0, 0});
 
   Plane * surface = new Plane(-3.);
 
@@ -22,17 +22,18 @@ Scene* initScene (){
   demoObjs->insertObject(e);
   demoObjs->insertObject(f);
   demoObjs->insertObject(surface);
-/*
-  for (int i = 0; i < 50; i++){
-    Sphere *x = new Sphere((vec3) {0.,0., (float)i*-4 + 4}, 2, (Color) {100,100,100});
+
+  for (int i = -50; i < 50; i++){
+    Sphere *x = new Sphere((vec3) {(float)i*-4 + 4, 4, -25}, 2, (Color) {100,100,100});
     demoObjs->insertObject(x);
 
   }
-  */
+  
 
   std::vector<PointLight>* demoLights = new std::vector<PointLight>();
   demoLights->push_back( (PointLight) { (vec3) {0., 10., 10.}, 0.7 } );
-  Camera* defaultCamera = new Camera((vec3) {0.,1.,0}, (vec3) {20,5.,20}, (vec3) {0.,0.,0.}, 0.78, width, height); // ~ pi/4
+  demoLights->push_back( (PointLight) { (vec3) {10., 10., 0.}, 0.7 } );
+  Camera* defaultCamera = new Camera((vec3) {0.,1.,0}, (vec3) {10,10,20}, (vec3) {0.,4.,0.}, 0.9, width, height); // ~ pi/4
   Scene *demoScene = new Scene(*defaultCamera, *demoObjs, *demoLights, 0.1, 2, width, height);
   return demoScene;
 }
